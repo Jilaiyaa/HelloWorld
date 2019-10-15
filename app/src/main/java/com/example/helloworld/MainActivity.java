f599c1859b778cb381a1e1e403b5ef3c00c5e1c6
Jilaiyaa <calvinzenz@gmail.com>
2019-10-15 07:29:23 +0200

    Start

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bOne;
+
+    private Button bFour;
+
+    private Button bSeven;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.bOne = this.findViewById(R.id.bOne);
+        this.bOne.setOnClickListener(this);
+
+        this.bFour = this.findViewById(R.id.bFour);
+        this.bFour.setOnClickListener(this);
+
+        this.bSeven = this.findViewById(R.id.bSeven);
+        this.bSeven.setOnClickListener(this);
+
+
     }
 }
